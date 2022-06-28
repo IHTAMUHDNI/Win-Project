e62ed0df673baae86c9f209dbbcc4d39b3645856
IHTAMUHDNI <108049641+IHTAMUHDNI@users.noreply.github.com>
2022-06-28 13:53:29 +0530

    changed the b value

--- Add.java
+++ Add.java
@@ -1,7 +1,7 @@
 import java.io.*;
 public class Add{
     public static void main(String args[]){
-        int a=1,b=20;
+        int a=1,b=20,b=30;
         int c=a+b;
         System.out.println("c value is  "+c);
     }
