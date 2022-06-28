3f5d16283aeee1fe150bf706e0217466217a165c
IHTAMUHDNI <108049641+IHTAMUHDNI@users.noreply.github.com>
2022-06-28 14:03:35 +0530

    Added new variable e

--- Add.java
+++ Add.java
@@ -1,8 +1,10 @@
 import java.io.*;
 public class Add{
     public static void main(String args[]){
-        int a=1,b=20;
+        int a=1,b=20,d=3;
         int c=a+b;
+        int e=d+a;
         System.out.println("c value is  "+c);
+        System.out.println("e value is "+e);
     }
 }
